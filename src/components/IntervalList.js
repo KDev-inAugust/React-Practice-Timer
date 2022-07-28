30e7ed37f54736f10a927699ca4df048d1cb050e
Kane Mathis <admin@kanemathis.com>
2022-07-28 14:08:40 -0400

    date display on date turnover function now working

--- src/components/IntervalList.js
+++ src/components/IntervalList.js
@@ -3,13 +3,30 @@ import Interval from "./Interval";
 
 function IntervalList ({intervalData}){
 
+
+
     return(
         <div className="container">
-        <div >{intervalData.map((index)=>{return(
+            
+            <div >
+            {intervalData.length===0 ? 'Loading': `Log Begin ${intervalData[0].date.day}/ ${intervalData[0].date.month}`}
+            {intervalData.map((index, cur)=>{
+            if (cur>0){cur=cur-1}
+            
+             return(
+            <div>
+                
+            <div key={index.name}>{intervalData[cur].date.day===index.date.day? null : `${index.date.day}/ ${index.date.month}`}</div>
             <Interval key={index.id} name={index.name} duration={index.duration} category={index.category}/>
-        )})}</div>
+            </div>
+            )})}
+            
+            </div>
         </div>
     )
 }
 
 export default IntervalList;
+
+
+
