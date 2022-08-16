25259fda5850c3afe5ef9c4260f2035ead55cb65
Kane Mathis <admin@kanemathis.com>
2022-08-16 11:15:56 -0400

    styled the clock

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Image from '../circleBorder.png';
 
 function Timer ({minutes, seconds, month, day, year, intervalStart})
 
 {
     return(
-        <div id="timer">
-            <div className="clock" >{`${minutes}:${seconds}`}</div>
-  <br/> 
- {`on:  ${month}/ ${day}/ ${year} at:${intervalStart}`}
+        <div>
+            <div id="timer">
+                <div className="clock" >
+                    <div id="clockNumbers">{`${minutes}:${seconds}`}</div>
+            </div>
+        </div>
+         <br/> 
+        {`on:  ${month}/ ${day}/ ${year} at:${intervalStart}`}
+        
         </div>
     )
 }
